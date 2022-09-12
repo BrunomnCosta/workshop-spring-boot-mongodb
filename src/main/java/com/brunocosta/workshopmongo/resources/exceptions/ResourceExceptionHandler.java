e4b84afef5b025ceffd34c3fd65f187de29f96a2
Bruno Costa <bruno.cst1@gmail.com>
2022-09-12 12:12:49 +0100

    Obtendo um usuario por id

---
+++ src/main/java/com/brunocosta/workshopmongo/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.brunocosta.workshopmongo.resources.exceptions;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.brunocosta.workshopmongo.services.exception.ObjectNotFoundException;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request ){
+		//system.currentTimeMillis da o tempo atual do sistema
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError(System.currentTimeMillis(), status.value(), "Não encontrado", e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+}
