fd63bff7b85f95acf88492a0a65eba975ca964b0
Bruno Costa <bruno.cst1@gmail.com>
2022-09-12 14:41:43 +0100

    Apagar usuario com DELETE

--- src/main/java/com/brunocosta/workshopmongo/services/UserService.java
+++ src/main/java/com/brunocosta/workshopmongo/services/UserService.java
@@ -13,24 +13,30 @@ import com.brunocosta.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository userRepository;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return userRepository.findAll();
 	}
-	
+
 	public User findById(String id) {
 		Optional<User> obj = userRepository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
-	
+
 	public User insert(User obj) {
 		return userRepository.insert(obj);
 	}
-	
+
+	public void delete(String id) {
+		findById(id);
+		userRepository.deleteById(id);
+	}
+
 	public User fromDTO(UserDTO objDto) {
 		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
+
 }
